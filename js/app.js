349b396be73c47cf444231373765a27e00f5d84a
AyahZaareer <ayah@LTUCSTUDENT-L075.localdomain>
2021-03-15 15:35:47 +0200

    let user to know about me

---
+++ js/app.js
@@ -0,0 +1,58 @@
+'use strict';
+let myname = prompt('What do you expect my Name  to be? is ayah! yes or no');
+if(myname.toLowerCase() === 'yes')
+{
+  alert('yes you are right');
+  console.log('yes my name is ayah');
+}else
+{
+  alert('you are wrong');
+}
+let myAge=prompt('Do you think that I am over 50? yes or no');
+if(myAge.toLowerCase() === 'no')
+{
+  alert('yes you are right');
+  console.log('yas my age is 26');
+}else
+{
+  alert('noooooooooo you are wrong');
+
+}
+let from=prompt('Where do you think I am from?maybe from Jordan n ,y');
+if(from.toUpperCase() === 'Y')
+{
+  alert('yes that right');
+  console.log('yes i am from jordan');
+}else
+{
+  alert('you are wrong');
+}
+let major=prompt('Do you think that my major is Communications Engineering? yes ,no');
+if(major.toUpperCase()=== 'YES')
+{
+  alert('Wow you are good');
+  console.log('yes I am communicatin engineer');
+}else
+{
+  alert('you do not gat the right answer');
+}
+let time=prompt('Do you think that my graduated yere is 2017? y ,n');
+if(time.toLowerCase() === 'y')
+{
+  alert('yes you are corrict');
+  console.log('yes I am graduated in 2017');
+}else
+{
+  alert('no you are wrong');
+}
+let userName=prompt('you know about me this time I would know about you , First of all what your name?');
+alert('Hi '+ userName);
+alert(' wlecoam '+userName +' Thank you for using my web site');
+
+
+
+
+
+
+
+
