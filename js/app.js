cac602dcc613eca705a9151bf8a17e6027057185
AyahZaareer <ayah@LTUCSTUDENT-L075.localdomain>
2021-03-18 13:14:04 +0200

    change on my repo

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 'use strict';
 let gread=0;
+
 function coderName(){
   let myname = prompt('What do you expect my Name  to be? is ayah! yes or no');
   if(myname.toLowerCase() === 'yes')
@@ -68,58 +69,70 @@ function coderYearGraduate(){
 }
 coderYearGraduate();
 
-function playerName(){
-  let userName=prompt('you know about me this time I would know about you , First of all what your name?');
-  alert('Hi '+ userName);
-  alert(' wlecoam '+userName +' Thank you for using my web site');
-  alert('There is an additional question, prepare for it');
-}
-playerName();
-
 function quizGame(){
-  let guess= prompt('Choose a number between 1 - 100');
-  let guessNumber= Number((guess));
+
+  let num=50;
   let a=1;
   for (let i=1;i<5;i++)
-  {
-    if (guess === '50')
-    {
-      alert('good ,50 is the right answer');
-      gread=gread+1;
-      break ;
-    }
-    else if(guessNumber > 50)
-    {
+  {let guess=Number( prompt('Choose a number between 1 - 100'));
+
+    if (guess < num)
+    {a=a+1;
+      alert('too low, Try again');
+      alert(' this your effot have num ' + a);
+
+      console.log(guess);
+
+    }else if ( guess > 50){
+
+
       a=a+1;
       alert('too hight,Try again');
       alert(' this your effot have num ' + a);
-      guessNumber=prompt('Choose  agian number between 1 - 100');
-      console.log(guessNumber);
+
+      console.log(guess);
     }
     else{
-      a=a+1;
-      alert('too low,Try again');
-      alert(' this your effot have num ' + a);
-      let guessNumber=prompt('Choose  agian number between 1 - 100 ' );
-      console.log(guessNumber);
+      alert('good ,50 is the right answer');
+      gread=gread+1;
+      break ;
+
     }
   }
+  alert('the correct answer to this question is 50 ,thank you for all .');
 }
 quizGame();
 function colorGame(){
-  let color = prompt('We have another question, what do you think is my favorite color?');
-  alert('dont forget ,you have 6 time to choise corrict answer');
-  { for (let i=2;i <= 7;i++)
-    if(color !== 'red' && color !== 'green' && color !=='blue')
-    { alert('try again , this affort have num: ' + i);
-      color = prompt(' what do you think is my favorite color?'); }
-    else
+
+  let favcolor=['red','green','blue'];
+  let ans=false;
+  for(let i =1;i<=6;i++){
+    let color = prompt(' what do you think is my favorite color?');
+    alert('dont forget ,you have 6 time to choise corrict answer'+ i);
+    { for (let i=0;i <favcolor.length;i++)
+      if(favcolor===color[i])
+      { alert('good ,the right answer');
+        ans=true;
+        gread=gread+1;
+
+        break;
+      }
+    }
+
+    if (ans===true)
     {
-      alert('good ,the right answer');
-      gread=gread+1;
       break;
     }
   }
+
+
+
+
+
+
+
+
+
   console.log(gread);
   alert('your gread in this game is : '+ gread);
   alert('Thank you for taking part in this game.');
